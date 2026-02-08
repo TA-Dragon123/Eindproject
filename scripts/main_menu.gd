0fbd27efee3839f5cdebeb06303b8489a5c96e41
TA-Dragon123 <tristan.ansingh@bazandpoort.be>
2026-02-08 15:55:16 +0100

    map but

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
 
 @onready var auto_scroller: Node2D = $AutoScroller
+@onready var button_container: VBoxContainer = $ScreenManager/ButtonContainer
+@onready var game_options_screen: Panel = $ScreenManager/GameOptionsScreen
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta: float) -> void:
 	pass
-
+	
+func _ready():
+	button_container.visible = true
+	game_options_screen.visible = false
 
 func _on_start_pressed() -> void:
 	auto_scroller.visible = false
@@ -18,8 +21,21 @@ func _on_start_pressed() -> void:
 
 
 func _on_game_options_pressed() -> void:
-	pass # Replace with function body.
+	button_container.visible = false
+	game_options_screen.visible = true
 
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_back_pressed() -> void:
+	button_container.visible = true
+	game_options_screen.visible = false
+
+
+func _on_map_1b_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_map_2b_pressed() -> void:
+	pass # Replace with function body.
